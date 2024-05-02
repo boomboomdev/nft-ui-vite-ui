c630728f100e9522dcbda8d945c85dacbbfa722c
Vander Moleker <noierdev@proton.me>
2024-05-02 14:45:22 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import { BrowserRouter } from "react-router-dom"
 import AppRoutes from './routes/index.tsx'
-import { Suspense } from "react"
 function App() {
 
   return (
     <>
         <div>
           <BrowserRouter>
-            <Suspense fallback={<></>} >
               <AppRoutes/>
-            </Suspense>
           </BrowserRouter>
         </div>
     </>
